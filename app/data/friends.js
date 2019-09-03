496bfb6fb0e78771eb2e6263e38e8d477db82d4d
Michal Terranova <michalterranova@mail.com>
2019-09-03 01:07:45 -0400

    html and some routes made

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Lex Luthor",
+        photo: "http://images4.wikia.nocookie.net/__cb20081007203815/superman/images/d/d8/Lex-luthor1.jpg",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }, 
+    {
+        name: "Selena Kyle",
+        photo: "http://basementrejects.com/wp-content/uploads/2014/08/catwoman-2004-movie-review-halle-berry-domino-mask-300x153.jpg",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ] 
+    }, 
+    {
+        name: "Harley Quinn",
+        photo: "https://livewallpaperhd.com/wp-content/uploads/2018/07/Harley-Quinn-Desktop-Backgrounds.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ] 
+    } 
+];
+ 
+module.exports = friends;
