3cf5464b2713f63734bfb1cbdde9e6b880f38e10
Michal Terranova <michalterranova@mail.com>
2019-09-03 01:06:53 -0400

    routes now connecting to html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var path = require("path");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8000; 
@@ -8,9 +8,15 @@ var PORT = process.env.PORT || 8000;
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(bodyParser.json({ type: "application/=+json"}));
-app.use(bodyParser.raw({ type: 'application/vmd.custom-type'}));
-app.use(bodyParser.text({ type: 'text/html'}));
+// Sets up the Express app to handle data parsing
+app.use(express.static("app/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen (PORT, function(){
     console.log("You are now connected to PORT: "+ PORT);
